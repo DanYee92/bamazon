59e974fce4368b5ade397c1144a7e7c2f846a10d
DanYee92 <danyee92@gmail.com>
2018-02-28 19:40:42 -0500

    Update bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,7 +4,7 @@ var inquirer = require('inquirer');
 
 
 
-// var columns = ['item_id', 'product_name','department_name','price','stock_quantity'];
+ var columns = ['item_id', 'product_name','department_name','price','stock_quantity'];
 
 	//establish connection
 	var connection = mysql.createConnection({
@@ -31,82 +31,6 @@ function displayTable(){
 		customerRequest(res);
 	})
 }
-//user can select and purchase which item to buy from the res
-// var customerRequest=function(res){
-// 	//create inquiry prompt to ask the customer for what item they would like to buy
-// 	//use array of question objects inside argument
-// 	inquirer.prompt([{
-// 		type: "input",
-// 		name: 'choice',
-// 		message: "Whats the id of the item you would like to purchase? ",
-//
-// 	}]).then(function(answer){
-// 		var correct = false;
-// 		for(var i=0;i<res.length;i++){
-// 			if(res[i].id == answer.choice){
-// 				correct=true;
-// 				var product=answer.choice;
-// 				var item_id=i;
-// 				inquirer.prompt({
-// 					type: "input",
-// 					name: "stocked",
-// 					message:"How many units would you like to buy?",
-// 					validate: function(value){
-// 						if(isNaN(value)==false){
-// 							return true;
-// 						}
-// 						else{
-// 							return false
-// 						}
-// 					}
-// 				}).then(function(answer){
-// 					if((res[item_id].stock_quantity-answer.stocked)>0){
-// 						connection.query("UPDATE products SET stock_quantity='"+(res[item_id].stock_quantity-answer.stocked)+"'WHERE product_name= '"+product+"'", function(err, res2){
-// 							console.log(parseInt(answer) * parseInt((res[item_id].price)));
-// 							displayTable();
-// 						});
-// 					}else{
-// 						console.log("not valid");
-// 						customerRequest(res2);
-// 					}
-//
-// 				})
-//
-// 			}
-// 		}
-// 		if(i==res.length && correct==false){
-// 			console.log("Insufficient quantity");
-// 			customerRequest(res);
-// 		}
-// 	})
-// }
-	// function customerRequest() {
-	// 	//Ask customer for id input
-	// 	inquirer.prompt([
-	// 				{
-	// 					type:'input',
-	// 					name: 'id',
-	// 					message: 'What is the ID of the product you would like to buy?',
-	// 					validate: function(value) {
-	// 						var valid = !isNaN(parseFloat(value));
-	// 						return valid || 'Please enter a number';
-	// 					}
-	// 				},
-	// 				{
-	// 					type:'input',
-	// 					name:'quantity',
-	// 					message:'How many would you like to buy?',
-	// 					validate: function(value) {
-	// 						var valid = !isNaN(parseFloat(value));
-	// 						return valid || 'Please enter a number';
-	// 					}
-	// 				}
-	// 	])
-	// 	.then(function(answer) {
-	// 		checkQuantity(answer.id, answer.quantity);
-	// 	});
-	// }
-
 
 
 function customerRequest() {
